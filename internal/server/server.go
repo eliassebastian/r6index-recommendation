f7442ca8369bde1734ac7fa612544f681601f3fb
Elias <eeschmoelz@gmail.com>
2023-04-12 23:46:13 +0100

    feat: grpc server service

---
+++ internal/server/server.go
@@ -0,0 +1,15 @@
+package server
+
+import (
+	"context"
+
+	pb "github.com/eliassebastian/r6index-recommendation/pkg/proto/server"
+)
+
+type RecommendationServer struct {
+	pb.UnimplementedRecommendationServiceServer
+}
+
+func (s *RecommendationServer) Index(ctx context.Context, in *pb.Request) (*pb.Response, error) {
+	return nil, nil
+}
