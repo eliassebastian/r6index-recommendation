435728fa34a6b5935fbed6172535289706f4b326
Elias <eeschmoelz@gmail.com>
2023-04-13 16:42:58 +0100

    feat: weaviate import batch unit test

--- internal/weaviate/weaviate_test.go
+++ internal/weaviate/weaviate_test.go
@@ -2,6 +2,7 @@ package weaviate
 
 import (
 	"context"
+	"log"
 	"reflect"
 	"testing"
 
@@ -79,6 +80,53 @@ func TestWeaviateData(t *testing.T) {
 		if !reflect.DeepEqual(arr, vec) {
 			t.Errorf("weaviate data getter error: got %v want %v", arr, vec)
 		}
+
+		cleanupSimpleTestClient(t, client)
+	})
+
+	t.Run("Test Batch Import Vector Object", func(t *testing.T) {
+		client := createSimpleTestClient()
+		createWeaviateTestSchemaWithVectorizorlessClass(t, client)
+
+		// sample batch data
+		data := []*models.Object{
+			{
+				ID:     "6844b415-aa94-43c9-8823-9389e4816910",
+				Vector: []float32{211.0, 0.76, 35.0, 3424.0},
+				Class:  "TestR6Index",
+			},
+			{
+				ID:     "6844b415-aa94-43c9-8823-9389e4816914",
+				Vector: []float32{250.0, 0.80, 35.0, 5000.0},
+				Class:  "TestR6Index",
+			},
+			{
+				ID:     "6844b415-aa94-43c9-8823-9389e4816923",
+				Vector: []float32{110.0, 0.43, 15.0, 1000.0},
+				Class:  "TestR6Index",
+			},
+			{
+				ID:     "6844b415-aa94-43c9-8823-9389e4816905",
+				Vector: []float32{300.0, 0.54, 17.0, 1233.0},
+				Class:  "TestR6Index",
+			},
+		}
+
+		batchR, err := client.Batch().ObjectsBatcher().WithObjects(data...).Do(context.Background())
+
+		if err != nil {
+			t.Errorf("weaviate batch creator error: got %v want nil", err)
+		}
+
+		if batchR == nil {
+			t.Errorf("weaviate batch creator error: got nil want not nil")
+		}
+
+		if len(batchR) != 4 {
+			t.Errorf("weaviate batch creator error: got %d want %d", len(batchR), 4)
+		}
+
+		log.Println(batchR)
 
 		cleanupSimpleTestClient(t, client)
 	})
